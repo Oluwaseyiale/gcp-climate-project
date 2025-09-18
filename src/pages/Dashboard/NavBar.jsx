f156916b61514f091ee530cf948e6620a7a469a7
joshuamondie <joshuamondie@gmail.com>
2025-09-18 17:02:11 +0100

    WIP - Enrolled courses fixed

--- src/pages/Dashboard/NavBar.jsx
+++ src/pages/Dashboard/NavBar.jsx
@@ -6,20 +6,19 @@ import logo from "../../assets/GCPlogo.png";
 export const NavBar = () => {
 	return (
 		<div className="flex  justify-between p-4   shadow-md w-full  items-center">
-			<img src={logo} alt="logo" width={80} />
-			<div className=" flex gap-8 ">
+			<Link to>
+				<img src={logo} alt="logo" width={80}  />
+
+			</Link>
+			<div className=" flex gap-8 mr-10">
 				<Link className=" font-figtree text-base font-normal text-[#000000]">
 					Notification
 				</Link>
-				<Link className=" font-figtree text-base font-normal text-[#000000]">
-					Support
-				</Link>
+
 				<Link className=" font-figtree text-base font-normal text-[#000000]">
 					Courses
 				</Link>
-				<Link className=" font-figtree text-base font-normal text-[#000000]">
-					Donate
-				</Link>
+
 			</div>
 		</div>
 	);
--- src/pages/courses/CourseDetails.jsx
+++ src/pages/courses/CourseDetails.jsx
@@ -2,68 +2,27 @@
 
 import { FaArrowRightLong } from "react-icons/fa6";
 import PropTypes from "prop-types";
-import { useState } from "react";
 import desktopImg from "../../assets/imgA.png";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useCourseId, useEnroll, useUserProfile } from "../../api/queries";
+import {  useNavigate, useParams } from "react-router-dom";
+import { useCourseId } from "../../api/queries";
 import Navbar from "../../components/navbarcomponent/Navbar";
 import Footer from "../../components/footercomponent/Footer";
-import { Dialog, DialogBody, DialogFooter } from "@material-tailwind/react";
 
 const CourseDetails = () => {
 	const { id } = useParams(); // Get course ID from URL
-	const { data: courseData } = useCourseId(id); // Fetch course details
-	const { data: userData } = useUserProfile(); // Fetch user profile
-	const { mutate: enroll, isPending } = useEnroll();
-	const [modal, setModal] = useState({
-		open: false,
-		message: "",
-		success: false,
-	});
+	const { data: courseData } = useCourseId(id);
+
+
 
 	const navigate = useNavigate();
 
-	// Extract course details
-	const course = courseData?.data?.id;
 	const price = courseData?.data?.price;
 	const experienceLevel = courseData?.data?.experience_level;
 	const schedule = courseData?.data?.schedule;
+	;
 
-	console.log("course", courseData);
 
-	// Extract user ID from profile
-	const user = userData?.data?.user;
 
-	const isUserLoggedIn = Boolean(user);
-
-	const handleEnroll = () => {
-		if (!isUserLoggedIn) {
-			setModal({
-				open: true,
-				message: "You must be logged in to enroll.",
-				success: false,
-			});
-			return;
-		}
-
-		enroll(
-			{ user, course },
-			{
-				onSuccess: () =>
-					setModal({
-						open: true,
-						message: "Enrollment Successful!",
-						success: true,
-					}),
-				onError: () =>
-					setModal({
-						open: true,
-						message: "Enrollment failed. Please try again.",
-						success: false,
-					}),
-			}
-		);
-	};
 
 	const desc = [
 		{ title: "Duration: 16hrs a week", description: "4 Weeks" },
@@ -86,14 +45,16 @@ const CourseDetails = () => {
 							{courseData?.data?.description}
 						</p>
 
-						<Link onClick={handleEnroll}>
+						<button onClick={() => {
+							navigate("/login");
+						}}>
 							<div className="border mt-4 bg-[#008056] h-9 w-36 rounded-lg flex items-center justify-center gap-4 font-figtree">
 								<h1 className="text-white">
-									{isPending ? "Enrolling..." : "Enroll Now"}
+									Enroll Now
 								</h1>
 								<FaArrowRightLong color="white" />
 							</div>
-						</Link>
+						</button>
 					</div>
 					<div className="rounded-full md:h-[270px] md:w-[270px] lg:h-[270px] lg:w-[270px] bg-[#008056] relative">
 						<img
@@ -119,52 +80,6 @@ const CourseDetails = () => {
 					))}
 				</div>
 			</div>
-
-			{/* Modal */}
-			<Dialog
-				open={modal.open}
-				handler={() => setModal({ open: false, message: "", success: false })}
-				size="xs"
-				className="p-4 w-[40%]"
-			>
-				<DialogBody>
-					<h2
-						className={`text-lg font-bold ${
-							modal.success ? "text-green-600" : "text-red-600"
-						}`}
-					>
-						{modal.success ? "Success" : "Error"}
-					</h2>
-					<p>{modal.message}</p>
-				</DialogBody>
-				<DialogFooter>
-					{/* <button
-						onClick={() =>
-							setModal({ open: false, message: "", success: false })
-						}
-						className="mt-3 bg-[#008056] text-white px-4 py-2 rounded"
-					>
-						Close
-					</button> */}
-
-					{modal.success ? (
-						<button
-							onClick={() => {
-								navigate("/login");
-							}}
-							className="mt-3 bg-[#008056] text-white px-4 py-2 rounded"
-						></button>
-					) : (
-						<button
-							onClick={() => {
-								navigate("/Signup");
-							}}
-							className="mt-3 bg-[#008056] text-white px-4 py-2 rounded"
-						></button>
-					)}
-				</DialogFooter>
-			</Dialog>
-
 			<Footer />
 		</div>
 	);
